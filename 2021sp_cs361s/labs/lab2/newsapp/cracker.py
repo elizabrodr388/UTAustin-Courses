5eabfec29a3f110351cfb4daa3339cd9d085e246
elizabrodr388 <elizabeth.amanda.rodriguez@gmail.com>
2021-03-10 20:56:20 -0500

    Half of part 2 working.

--- 2021sp_cs361s/labs/lab2/newsapp/cracker.py
+++ 2021sp_cs361s/labs/lab2/newsapp/cracker.py
@@ -4,6 +4,7 @@ import pandas as pd
 import base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import hashlib
 
 # if there is no arguments
 if len(sys.argv) == 1:
@@ -25,12 +26,33 @@ if len(sys.argv) == 1:
 
     con.close()
 
-    # take each splashdata password
-    with open('splashdata.txt', 'r') as f:
-        com_pwd = []
-        for line in f:
-            print(line)
-            com_pwd.append(line)
+    # list of common passwords
+    com_pwd = [
+    "123456",
+    "123456789",
+    "qwerty",
+    "password",
+    "1234567",
+    "12345678",
+    "12345",
+    "iloveyou",
+    "111111",
+    "123123",
+    "abc123",
+    "qwerty123",
+    "1q2w3e4r",
+    "admin",
+    "qwertyuiop",
+    "654321",
+    "555555",
+    "lovely",
+    "7777777",
+    "welcome",
+    "888888",
+    "princess",
+    "dragon",
+    "password1",
+    "123qwe"]
     
     # check to see if cracked password
     for common in com_pwd:
@@ -39,12 +61,24 @@ if len(sys.argv) == 1:
             full_password = user_info[username]
 
             algo = full_password[ui_algorithm]
-            iters = full_password[ui_iterations]
-            salt = full_password[ui_salt]
+            iters = int(full_password[ui_iterations])
+            salt = str.encode(full_password[ui_salt])
+            h = str.encode(full_password[ui_hash])
             
-            kdf = PBKDF2HMAC (
-                algorithm = algo,
-                length = 32,
-                salt = salt,
-                iterations = iters,
-            )
+            if algo == "pbkdf2_sha256":
+                kdf = PBKDF2HMAC (
+                    algorithm = hashes.SHA256(),
+                    length = 32,
+                    salt = salt,
+                    iterations = iters,
+                )
+
+                b_key = kdf.derive(str.encode(common)) # bytes
+                key = base64.b64encode(b_key)
+
+                if h == key:
+                    print(str(username) + "," + str(common))
+
+                # print("COMMON PASSWORD HASH  : " + str(key))
+                # print("USER PASSWORD HASH    : " + str(h))
+                # print("\n")
