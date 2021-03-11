3ca09624dc52ba8eb7bc883b4cdf99c1ff928a26
elizabrodr388 <elizabeth.amanda.rodriguez@gmail.com>
2021-03-10 21:48:25 -0500

    All tests passing

--- 2021sp_cs361s/labs/lab2/newsapp/cracker.py
+++ 2021sp_cs361s/labs/lab2/newsapp/cracker.py
@@ -6,79 +6,139 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import hashlib
 
-# if there is no arguments
-if len(sys.argv) == 1:
+# indices for user_info value (password)
+ui_algorithm = 0
+ui_iterations = 1
+ui_salt = 2
+ui_hash = 3
 
-    # indices for user_info value (password)
-    ui_algorithm = 0
-    ui_iterations = 1
-    ui_salt = 2
-    ui_hash = 3
+def main ():
+    # if there is no arguments
+    if len(sys.argv) == 1:
 
-    # open the sqllite3 database
-    con = sqlite3.connect('db.sqlite3')
-    cur = con.cursor()
+        #print("in top")
 
-    # extract all of the stored passwords hashes from db into dict
-    user_info = {}
-    for row in cur.execute('SELECT * from auth_user;'):
-        user_info[row[4]] = row[1].split("$")
+        # open the sqllite3 database
+        con = sqlite3.connect('db.sqlite3')
+        cur = con.cursor()
 
-    con.close()
+        # extract all of the stored passwords hashes from db into dict
+        user_info = {}
+        for row in cur.execute('SELECT * from auth_user;'):
+            user_info[row[4]] = row[1].split("$")
 
-    # list of common passwords
-    com_pwd = [
-    "123456",
-    "123456789",
-    "qwerty",
-    "password",
-    "1234567",
-    "12345678",
-    "12345",
-    "iloveyou",
-    "111111",
-    "123123",
-    "abc123",
-    "qwerty123",
-    "1q2w3e4r",
-    "admin",
-    "qwertyuiop",
-    "654321",
-    "555555",
-    "lovely",
-    "7777777",
-    "welcome",
-    "888888",
-    "princess",
-    "dragon",
-    "password1",
-    "123qwe"]
-    
-    # check to see if cracked password
-    for common in com_pwd:
-        for username in user_info:
+        con.close()
 
-            full_password = user_info[username]
+        # list of common passwords
+        com_pwd = [
+        "123456",
+        "123456789",
+        "qwerty",
+        "password",
+        "1234567",
+        "12345678",
+        "12345",
+        "iloveyou",
+        "111111",
+        "123123",
+        "abc123",
+        "qwerty123",
+        "1q2w3e4r",
+        "admin",
+        "qwertyuiop",
+        "654321",
+        "555555",
+        "lovely",
+        "7777777",
+        "welcome",
+        "888888",
+        "princess",
+        "dragon",
+        "password1",
+        "123qwe"]
+        
+        # check to see if cracked password
+        for common in com_pwd:
+            for username in user_info:
 
-            algo = full_password[ui_algorithm]
-            iters = int(full_password[ui_iterations])
-            salt = str.encode(full_password[ui_salt])
-            h = str.encode(full_password[ui_hash])
-            
-            if algo == "pbkdf2_sha256":
-                kdf = PBKDF2HMAC (
-                    algorithm = hashes.SHA256(),
-                    length = 32,
-                    salt = salt,
-                    iterations = iters,
-                )
+                full_password = user_info[username]
 
-                b_key = kdf.derive(str.encode(common)) # bytes
-                key = base64.b64encode(b_key)
+                algo = full_password[ui_algorithm]
+                iters = int(full_password[ui_iterations])
+                salt = str.encode(full_password[ui_salt])
+                h = str.encode(full_password[ui_hash])
+                
+                if algo == "pbkdf2_sha256":
+                    kdf = PBKDF2HMAC (
+                        algorithm = hashes.SHA256(),
+                        length = 32,
+                        salt = salt,
+                        iterations = iters,
+                    )
 
-                if h == key:
-                    print(str(username) + "," + str(common))
+                    b_key = kdf.derive(str.encode(common)) # bytes
+                    key = base64.b64encode(b_key)
 
-                # print("COMMON PASSWORD HASH  : " + str(key))
-                # print("USER PASSWORD HASH    : " + str(h))
-                # print("\n")
+                    if h == key:
+                        print(str(username) + "," + str(common))
+
+    elif len(sys.argv) > 1:
+        
+        full_password = (sys.argv[1]).split("$")
+        algo = full_password[ui_algorithm]
+        iters = int(full_password[ui_iterations])
+        salt = str.encode(full_password[ui_salt])
+        h = str.encode(full_password[ui_hash])
+
+        if iters != 1:
+            print("Cannot brute-force password in time.")
+
+        else:
+            alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+            'm', 'n', 'o', 'p' 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+            cont = True
+            for a1 in alpha:
+                if not cont: break
+                c1 = a1
+                cont = checkPassword(c1, algo, iters, salt, h)
+
+                for a2 in alpha:
+                    if not cont: break
+                    c2 = c1 + a2
+                    cont = checkPassword(c2, algo, iters, salt, h)
+
+                    for a3 in alpha:
+                        if not cont: break
+                        c3 = c2 + a3
+                        cont = checkPassword(c3, algo, iters, salt, h)
+
+                        for a4 in alpha:
+                            if not cont: break
+                            c4 = c3 + a4
+                            cont = checkPassword(c4, algo, iters, salt, h)
+
+def checkPassword (my_str, algo, iters, salt, h):
+    print("IN CHECK PASSWORD")
+    if algo == "pbkdf2_sha256":
+
+        kdf = PBKDF2HMAC (
+            algorithm = hashes.SHA256(),
+            length = 32,
+            salt = salt,
+            iterations = iters,
+        )
+
+        b_key = kdf.derive(str.encode(my_str))
+        key = base64.b64encode(b_key)
+
+        print(str(h))
+        print(str(key))
+
+        if h == key:
+            print("Password cracked: \'" + my_str + "\'")
+            return False
+
+        return True
+
+main ()
