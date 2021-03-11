e8e082e7b0dfed36b15d69202a4704cdf96e1125
elizabrodr388 <elizabeth.amanda.rodriguez@gmail.com>
2021-03-10 22:24:05 -0500

    Fixed part 3, everything passing.

--- 2021sp_cs361s/labs/lab2/newsapp/cracker.py
+++ 2021sp_cs361s/labs/lab2/newsapp/cracker.py
@@ -5,6 +5,7 @@ import base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import hashlib
+import string
 
 # indices for user_info value (password)
 ui_algorithm = 0
@@ -94,29 +95,24 @@ def main ():
             print("Cannot brute-force password in time.")
 
         else:
-            alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-            'm', 'n', 'o', 'p' 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            for combo in create_combos ():
+                done = checkPassword(combo, algo, iters, salt, h)
+                if done: break
 
-            cont = True
-            for a1 in alpha:
-                if not cont: break
-                c1 = a1
-                cont = checkPassword(c1, algo, iters, salt, h)
 
-                for a2 in alpha:
-                    if not cont: break
-                    c2 = c1 + a2
-                    cont = checkPassword(c2, algo, iters, salt, h)
 
-                    for a3 in alpha:
-                        if not cont: break
-                        c3 = c2 + a3
-                        cont = checkPassword(c3, algo, iters, salt, h)
 
-                        for a4 in alpha:
-                            if not cont: break
-                            c4 = c3 + a4
-                            cont = checkPassword(c4, algo, iters, salt, h)
+
+def create_combos ():
+    alpha = list(string.ascii_lowercase)
+    for a in alpha:
+        yield a
+        for b in alpha:
+            yield a + b
+            for c in alpha:
+                yield a + b + c
+                for d in alpha:
+                    yield a + b + c + d
 
 def checkPassword (my_str, algo, iters, salt, h):
     
@@ -137,8 +133,8 @@ def checkPassword (my_str, algo, iters, salt, h):
 
         if h == key:
             print("Password cracked: \'" + my_str + "\'")
-            return False
+            return True
 
-        return True
+        return False
 
 main ()
